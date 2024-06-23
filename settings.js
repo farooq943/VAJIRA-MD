031bff2924e61c935a27e4b69f8b8ecfb6e428d4
rashmika123456 <101487048+rashmika123456@users.noreply.github.com>
2024-06-23 14:12:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94717775628' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=rRVyTboa#xVpT9YWy_yFtTZ5o3lhgAApliTcburVKlu8OZCoB3k8' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=qd8AES4Q#387lg86iKUHDrBfUWvrLVoRSRJ1_u14Ej7WTWa9OL2M' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
