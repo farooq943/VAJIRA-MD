421ecac9ae0cf54db5aef4754015dcfd627c5d60
farooq943 <162565404+farooq943@users.noreply.github.com>
2024-08-02 22:17:31 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '𝟵𝟮𝟯𝟯𝟯𝟮𝟱𝟵𝟭𝟵𝟰𝟯' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=LF0UUYJD#uPG3D8MQJ9Bt89F8lyB2voaNCrY0n-jyF7U-8w10A2M' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=zVV1wbQJ#RgKoQctM_tWqj4feauKIftMu4FcdesrOcONsfYH3Yrw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
